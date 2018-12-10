a1b3aa17ca729efbe8cf22b81751f15aaf21fe1a
Simon-Oliver <stauffer.simon@outlook.com>
2018-12-10 18:36:23 +1100

    Scope Part 1

---
+++ basics/scope.js
@@ -0,0 +1,16 @@
+const varOne = 'varOne';
+
+if (true) {
+  console.log(varOne);
+  const varTwo = 'varTwo';
+
+  if (true) {
+    const varFour = 'varFour';
+  }
+}
+
+if (true) {
+  const varThree = 'varThree';
+}
+
+console.log(varTwo);
