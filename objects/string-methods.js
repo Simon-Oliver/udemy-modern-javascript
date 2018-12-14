c4a7112d36fbd0f7f754538c946ffcb652f19581
Simon-Oliver <stauffer.simon@outlook.com>
2018-12-14 23:33:48 +1100

    string lession

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -1,2 +1,15 @@
-const name = 'Max Muster';
-name.length;
+const nameFirst = 'Max Muster';
+console.log(nameFirst.toUpperCase());
+
+const password = 'abc123password0987';
+
+console.log(password.includes('password'));
+
+const whitespace = '         James     ';
+console.log(whitespace.trim());
+
+function isValidPw(pwString) {
+  return pwString.length > 8 && !pwString.includes('password');
+}
+
+console.log(isValidPw('hffh'));
