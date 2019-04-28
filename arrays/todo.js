d04a0640549a93e9ece2db150c9bbe848c1ac229
Simon-Oliver <stauffer.simon@outlook.com>
2019-04-28 12:06:32 +1000

    fixed linter issue

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,4 +1,22 @@
-const toDo = ['Walk the dog', 'Do washing', 'Clean car', 'Random Todo', 'Last todo'];
+const toDo = [
+  {},
+  {
+    title: 'Todo 1',
+    completed: false
+  },
+  {
+    title: 'Todo 2',
+    complete: true
+  },
+  {
+    title: 'Todo 3',
+    complete: false
+  },
+  {
+    title: 'Todo 4',
+    complete: false
+  }
+];
 
 // const todoSum = function(arr) {
 //   const allTodo = `You have ${arr.length} item${
@@ -11,10 +29,15 @@ const toDo = ['Walk the dog', 'Do washing', 'Clean car', 'Random Todo', 'Last to
 //   console.log(secondLast);
 // };
 
-toDo.splice(2, 1);
-toDo.push('New last Todo');
-toDo.shift();
+// console.log(toDo.indexOf('Clean car'));
 
-toDo.forEach((e, i) => console.log(`${i +1}. ${e}`));
+// toDo.splice(2, 1);
+// toDo.push('New last Todo');
+// toDo.shift();
+
+// toDo.forEach((e, i) => console.log(`${i + 1}. ${e}`));
 
 // todoSum(toDo);
+
+console.log(toDo.indexOf({}));
+const saimon = 123;
