17c9406a67d117829d156ae4fffc5f3d7099ec4d
Simon-Oliver <stauffer.simon@outlook.com>
2019-05-15 19:59:28 +1000

    refactored and changed hangman code, added keydown listener

--- hangman/app.js
+++ hangman/app.js
@@ -1,6 +1,70 @@
-const product = {
-  name: 'Harry Potter'
+const Hangman = function(word, guesses) {
+  this.word = word;
+  this.guesses = guesses;
+
+  this.remainingGuesses = guesses;
+  this.guessedLetters = [];
+  this.letterArray = this.word.toLowerCase().split('');
 };
 
-console.log(product.hasOwnProperty('hasOwnProperty'));
-console.log(product);
+Hangman.prototype.play = function(guessedLetter) {
+  guessedLetter = guessedLetter.toLowerCase();
+  const isUnique = !this.guessedLetters.includes(guessedLetter);
+  const isBadGuess = !this.letterArray.includes(guessedLetter);
+  const isGameOver = this.remainingGuesses <= 0;
+
+  if (isGameOver) {
+    return 'GAME OVER!!!';
+  }
+
+  if (isUnique && !isBadGuess) {
+    console.log(
+      `Well Done!!! ---- You have ${this.remainingGuesses} guess${
+        this.remainingGuesses > 1 ? 'es' : ''
+      } remaining`
+    );
+  }
+
+  if (isUnique) {
+    this.guessedLetters.push(guessedLetter);
+  }
+
+  if (isUnique && isBadGuess) {
+    this.remainingGuesses--;
+    console.log(
+      `WRONG!!! ---- You have ${this.remainingGuesses} guess${
+        this.remainingGuesses > 1 ? 'es' : ''
+      } remaining`
+    );
+  }
+
+  return this.displayString().includes('*')
+    ? this.displayString()
+    : `
+------------
+${this.displayString()}
+YOU WIN!!!
+------------
+`;
+};
+
+Hangman.prototype.displayString = function() {
+  let string = '';
+  this.letterArray.forEach(letter => {
+    if (this.guessedLetters.includes(letter)) {
+      string += letter;
+    } else if (letter === ' ') {
+      string += ' ';
+    } else if (!this.guessedLetters.includes(letter)) {
+      string += '*';
+    }
+  });
+  return string;
+};
+
+const game1 = new Hangman('game', 4);
+const game2 = new Hangman('silly', 3);
+
+window.addEventListener('keydown', e => {
+  console.log(game1.play(e.key));
+});
