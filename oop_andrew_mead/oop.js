94eb348939ce54c69541f3622f6ac90c0b704c1c
Simon-Oliver <stauffer.simon@outlook.com>
2019-05-17 18:27:50 +1000

    explored subclasses

--- oop_andrew_mead/oop.js
+++ oop_andrew_mead/oop.js
@@ -1,4 +1,4 @@
-class PersonClass {
+class Person {
   constructor(firstName, lastName, age, likes = []) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -21,29 +21,29 @@ class PersonClass {
   }
 }
 
-const myPerson = new PersonClass('Urs', 'Fischer', 30);
+const myPerson = new Person('Urs', 'Fischer', 30);
 console.log(myPerson.getBio());
 
-const Person = function(firstName, lastName, age, likes = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.likes = likes;
-};
+// const Person = function(firstName, lastName, age, likes = []) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.likes = likes;
+// };
 
-Person.prototype.getBio = function() {
-  let bio = `${this.firstName} is ${this.age}.`;
-  this.likes.forEach(like => {
-    bio += ` ${this.firstName} likes ${like}.`;
-  });
-  return bio;
-};
+// Person.prototype.getBio = function() {
+//   let bio = `${this.firstName} is ${this.age}.`;
+//   this.likes.forEach(like => {
+//     bio += ` ${this.firstName} likes ${like}.`;
+//   });
+//   return bio;
+// };
 
-Person.prototype.setName = function(fullName) {
-  const nameArr = fullName.split(' ');
-  this.firstName = nameArr[0];
-  this.lastName = nameArr[1];
-};
+// Person.prototype.setName = function(fullName) {
+//   const nameArr = fullName.split(' ');
+//   this.firstName = nameArr[0];
+//   this.lastName = nameArr[1];
+// };
 
 const person1 = new Person('Horton', 'test', 68, ['drawing', 'math']);
 const person2 = new Person('Urs', 'test', 32);
@@ -53,3 +53,17 @@ person2.setName('Willy Wonka');
 
 console.log(person1.getBio());
 console.log(person2.getBio());
+
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes = []) {
+    super(firstName, lastName, age, likes);
+    this.position = position;
+  }
+
+  getBio() {
+    return `${this.firstName} is a ${this.position}.`;
+  }
+}
+
+const emp1 = new Employee('Max', 'Mustermann', 68, 'Chef', ['Reading', 'Cooking']);
+console.log(emp1.getBio());
