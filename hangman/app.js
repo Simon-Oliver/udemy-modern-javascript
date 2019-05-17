3dcdbed643f3fa0fd717a489378cb7f9233fac4f
Simon-Oliver <stauffer.simon@outlook.com>
2019-05-17 18:05:02 +1000

    changed hangman to use class syntax

--- hangman/app.js
+++ hangman/app.js
@@ -1,59 +1,61 @@
-const Hangman = function(word, guesses) {
-  this.word = word;
-  this.guesses = guesses;
+class Hangman {
+  constructor(word, guesses) {
+    this.word = word;
+    this.guesses = guesses;
 
-  this.remainingGuesses = guesses;
-  this.guessedLetters = [];
-  this.letterArray = this.word.toLowerCase().split('');
-  this.isGameOver = false;
-};
-
-Hangman.prototype.play = function(guessedLetter) {
-  guessedLetter = guessedLetter.toLowerCase();
-  const isUnique = !this.guessedLetters.includes(guessedLetter);
-  const isBadGuess = !this.letterArray.includes(guessedLetter);
-  this.isGameOver = this.remainingGuesses <= 0;
-
-  if (isUnique && !isBadGuess) {
-    console.log(
-      `Well Done!!! ---- You have ${this.remainingGuesses} guess${
-        this.remainingGuesses > 1 ? 'es' : ''
-      } remaining`
-    );
+    this.remainingGuesses = guesses;
+    this.guessedLetters = [];
+    this.letterArray = this.word.toLowerCase().split('');
+    this.isGameOver = false;
   }
 
-  if (isUnique) {
-    this.guessedLetters.push(guessedLetter);
-  }
+  play(guessedLetter) {
+    guessedLetter = guessedLetter.toLowerCase();
+    const isUnique = !this.guessedLetters.includes(guessedLetter);
+    const isBadGuess = !this.letterArray.includes(guessedLetter);
+    this.isGameOver = this.remainingGuesses <= 0;
 
-  if (isUnique && isBadGuess) {
-    this.remainingGuesses--;
-  }
-};
-
-Hangman.prototype.displayString = function() {
-  let string = '';
-  this.letterArray.forEach(letter => {
-    if (this.guessedLetters.includes(letter)) {
-      string += letter;
-    } else if (letter === ' ') {
-      string += ' ';
-    } else if (!this.guessedLetters.includes(letter)) {
-      string += '*';
+    if (isUnique && !isBadGuess) {
+      console.log(
+        `Well Done!!! ---- You have ${this.remainingGuesses} guess${
+          this.remainingGuesses > 1 ? 'es' : ''
+        } remaining`
+      );
     }
-  });
 
-  if (!this.isGameOver) {
-    if (string.includes('*')) {
-      return string;
+    if (isUnique) {
+      this.guessedLetters.push(guessedLetter);
     }
-    return `${string}
-    YOU WIN!!!`;
+
+    if (isUnique && isBadGuess) {
+      this.remainingGuesses--;
+    }
   }
-  if (this.isGameOver) {
-    return 'GAME OVER!!!';
+
+  displayString() {
+    let string = '';
+    this.letterArray.forEach(letter => {
+      if (this.guessedLetters.includes(letter)) {
+        string += letter;
+      } else if (letter === ' ') {
+        string += ' ';
+      } else if (!this.guessedLetters.includes(letter)) {
+        string += '*';
+      }
+    });
+
+    if (!this.isGameOver) {
+      if (string.includes('*')) {
+        return string;
+      }
+      return `${string}
+      YOU WIN!!!`;
+    }
+    if (this.isGameOver) {
+      return 'GAME OVER!!!';
+    }
   }
-};
+}
 
 const game1 = new Hangman('game', 4);
 const game2 = new Hangman('silly', 3);
