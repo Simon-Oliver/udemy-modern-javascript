ad4e44b1699b16e18c61f3cba1c29ebd7f2d3cf8
Simon-Oliver <stauffer.simon@outlook.com>
2019-05-17 18:01:10 +1000

    explored classes

--- oop_andrew_mead/oop.js
+++ oop_andrew_mead/oop.js
@@ -1,3 +1,29 @@
+class PersonClass {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
+
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`;
+    this.likes.forEach(like => {
+      bio += ` ${this.firstName} likes ${like}.`;
+    });
+    return bio;
+  }
+
+  setName(fullName) {
+    const nameArr = fullName.split(' ');
+    this.firstName = nameArr[0];
+    this.lastName = nameArr[1];
+  }
+}
+
+const myPerson = new PersonClass('Urs', 'Fischer', 30);
+console.log(myPerson.getBio());
+
 const Person = function(firstName, lastName, age, likes = []) {
   this.firstName = firstName;
   this.lastName = lastName;
