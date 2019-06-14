cc030ad2b847dbcff13a54994ebb356dbce5a11b
Simon-Oliver <stauffer.simon@outlook.com>
2019-06-14 21:51:22 +1000

    Promises

---
+++ promises/promises.js
@@ -0,0 +1,27 @@
+// Callback
+
+const getDataCallBack = callaback => {
+  setTimeout(() => {
+    callaback(undefined, 'This is the data');
+  }, 3000);
+};
+
+getDataCallBack((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+// Promise
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is the promise data');
+  }, 3000);
+});
+
+myPromise.then(res => {
+  console.log(res);
+});
