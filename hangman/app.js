135597d515b40b3e2f9bee84209e0d77a362baa3
Simon-Oliver <stauffer.simon@outlook.com>
2019-06-16 22:44:14 +1200

    refactored callback to promise api

--- hangman/app.js
+++ hangman/app.js
@@ -1,29 +1,18 @@
-getPuzzle('1', (error, word) => {
-  if (error) {
-    console.log(`Error: ${error}`);
-  } else {
-    console.log(word);
-    const game1 = new Hangman(word, 4);
+const game1 = new Hangman('Hello Chicka', 4);
 
-    const stringDIV = document.querySelector('.string');
-    stringDIV.textContent = game1.displayString();
+const stringDIV = document.querySelector('.string');
+stringDIV.textContent = game1.displayString();
 
-    const guessesDIV = document.querySelector('.guesses');
-    guessesDIV.textContent = game1.remainingGuesses;
+const guessesDIV = document.querySelector('.guesses');
+guessesDIV.textContent = game1.remainingGuesses;
 
-    window.addEventListener('keydown', e => {
-      game1.play(e.key);
-      console.log(game1);
-      stringDIV.textContent = game1.displayString();
-      guessesDIV.textContent = game1.remainingGuesses;
-    });
-  }
+window.addEventListener('keydown', e => {
+  game1.play(e.key);
+  console.log(game1);
+  stringDIV.textContent = game1.displayString();
+  guessesDIV.textContent = game1.remainingGuesses;
 });
 
-getCountry('CH', (err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(res.name);
-  }
-});
+getPuzzle('1');
+
+getCountry('CH').then(data => console.log(data.name));
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,29 +1,46 @@
-const getPuzzle = (wordCount, callback) => {
-  const request = new XMLHttpRequest();
-  request.addEventListener('readystatechange', e => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      callback(undefined, JSON.parse(e.target.response).puzzle);
-    } else if (e.target.readyState === 4) {
-      callback(JSON.parse(e.target.response).error, undefined);
-    }
-  });
-  request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
-  request.send();
-};
-
-const getCountry = (country, callback) => {
-  const countries = new XMLHttpRequest();
-  const countryCode = country;
-
-  countries.addEventListener('readystatechange', e => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      const data = JSON.parse(e.target.response).find(item => item.alpha2Code === countryCode);
-      callback(undefined, data);
-    } else if (e.target.readyState === 4) {
-      callback(e.target.response);
-    }
+const getPuzzle = wordCount =>
+  new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', e => {
+      if (e.target.readyState === 4 && e.target.status === 200) {
+        resolve(JSON.parse(e.target.response).puzzle);
+      } else if (e.target.readyState === 4) {
+        reject(JSON.parse(e.target.response).error);
+      }
+    });
+    request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+    request.send();
   });
 
-  countries.open('GET', 'https://restcountries.eu/rest/v2/all');
-  countries.send();
-};
+// Callback example
+
+// const getPuzzle = (wordCount, callback) => {
+//   const request = new XMLHttpRequest();
+//   request.addEventListener('readystatechange', e => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//       callback(undefined, JSON.parse(e.target.response).puzzle);
+//     } else if (e.target.readyState === 4) {
+//       callback(JSON.parse(e.target.response).error, undefined);
+//     }
+//   });
+//   request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+//   request.send();
+// };
+
+const getCountry = country =>
+  new Promise((resolve, reject) => {
+    const countries = new XMLHttpRequest();
+    const countryCode = country;
+
+    countries.addEventListener('readystatechange', e => {
+      if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.response).find(item => item.alpha2Code === countryCode);
+        resolve(data);
+      } else if (e.target.readyState === 4) {
+        reject(e.target.response);
+      }
+    });
+
+    countries.open('GET', 'https://restcountries.eu/rest/v2/all');
+    countries.send();
+  });
